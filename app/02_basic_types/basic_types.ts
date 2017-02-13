e0128087296a46c508a4db7ec080acb5be46959c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-13 15:33:26 +0100

    add: some test in 02

--- app/02_basic_types/basic_types.ts
+++ app/02_basic_types/basic_types.ts
@@ -19,3 +19,29 @@ console.log('sentence: ', sentence);
 // Array of given type
 let fibonacciArray : number[] = [1, 2, 3, 5, 8, 13, 21, 34];
 console.log('fibonacciArray', fibonacciArray);
+let syracuse14Array : Array<number> = [14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1, 4, 2, 1];
+console.log('syracuse14Array', syracuse14Array);
+
+// Tuple test
+let couple : [string, number] = ['Jean-Louis', 42];
+
+couple[2] = 'kiki';
+couple[2] = 13;
+// wrong: couple[2] = true;
+console.log('couple', couple);
+
+// Enum test
+enum State {Normal, Edition, Error};
+let s : number = State.Normal;
+console.log('s', s);
+let str : string = State[s];
+console.log('str', str);
+
+// Any test
+let x : any = 'coucou';
+console.log('x', x);
+x = true;
+console.log('x', x);
+x = 13;
+console.log('x', x);
+
